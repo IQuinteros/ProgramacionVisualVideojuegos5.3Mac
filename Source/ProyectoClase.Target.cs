73f4888be214bc93de2ef9277990794af1c3e7aa
Ignacio Quinteros <i.quinteros@hotmail.com>
2024-05-25 16:53:05 -0400

    Actualizacion para 5.4

--- Source/ProyectoClase.Target.cs
+++ Source/ProyectoClase.Target.cs
@@ -8,7 +8,7 @@ public class ProyectoClaseTarget : TargetRules
 	public ProyectoClaseTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "ProyectoClase" } );
 	}
--- Source/ProyectoClaseEditor.Target.cs
+++ Source/ProyectoClaseEditor.Target.cs
@@ -8,7 +8,7 @@ public class ProyectoClaseEditorTarget : TargetRules
 	public ProyectoClaseEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "ProyectoClase" } );
 	}
